84d64fa2874cb4b1c202f5e6932fe292dceadc76
elkadhi-oussama <elkadhioussama@yahoo.com>
2024-10-25 23:17:01 +0100

    update

--- API/index.js
+++ API/index.js
@@ -8,7 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://enig.vercel.app", // replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // specify methods to allow
+    credentials: true, // allow credentials if needed
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB Atlas
